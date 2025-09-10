d315a8321f3c2ffa9b2442d6101fa1a4199c5794
Kurimasu Tanaka <kamyshek185@gmail.com>
2025-09-10 07:01:16 +0300

    Login and Registration page improved

--- Tasker.DataAccess/DataTransferObjects/Auth/RegisterModel.cs
+++ Tasker.DataAccess/DataTransferObjects/Auth/RegisterModel.cs
@@ -2,4 +2,9 @@ using System;
 
 namespace Tasker.DataAccess.Auth;
 
-public class RegisterModel { public string Email { get; set; } public string Password { get; set; } }
+public class RegisterModel {
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Password { get; set; }
+}
